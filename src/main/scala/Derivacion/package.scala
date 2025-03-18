56f57f3ecf312bde0dd8f97bd4bc328920a5f4b6
Maria Juliana <mjsaatorres@gmail.com>
2025-03-18 12:31:10 -0500

    merge

--- src/main/scala/Derivacion/package.scala
+++ src/main/scala/Derivacion/package.scala
@@ -10,16 +10,13 @@ package object Derivacion {
 
   //forma anonima
 
-  def derivada2(f: Double => Double): Double => Double = { x =>
+  def derivada2(f: Double => Double): Double => Double = { (x: Double) =>
     val h = 0.1
     (f(x - 2 * h) - 8 * f(x - h) + 8 * f(x + h) - f(x + 2 * h)) / (12 * h)
   }
 
-  def derivadaSuma(f: Double => Double, g: Double => Double): Double => Double = {
-
-    def calcularSumaDerivada(x: Double): Double = derivada(f)(x) + derivada(g)(x)
-
-    calcularSumaDerivada
+  def derivadaSuma(f: Double => Double, g: Double => Double): Double => Double = { (x:Double) =>
+    derivada(f)(x) + derivada(g)(x)
   }
 
   def derivadaResta(f: Double => Double, g: Double => Double): Double => Double = {
